499c983e8064ade77515a5da87f2a04e446c3e82
eugeneswalker <eugenesunsetwalker@gmail.com>
2019-10-16 16:22:44 -0700

    update ubi7 ppc64le base recipe

--- docker-recipes/ubi7/ppc64le/base/build.sh
+++ docker-recipes/ubi7/ppc64le/base/build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#You may want to change the image name for personal use.
 docker build -t ecpe4s/ubi7_ppc64le_base:1.1 .
---
+++ docker-recipes/ubi7/ppc64le/base/modules/bashrc_addendum.sh
@@ -0,0 +1,11 @@
+if [ -z "$BASHRC_READ" ]; then
+  export BASHRC_READ=1
+  source ${SPACK_ROOT}/share/spack/setup-env.sh
+  source `spack location -i lmod`/lmod/lmod/init/bash
+  module purge
+  module unuse /spack/share/spack/modules/linux-rhel7-ppc64le
+  module use /spack/share/spack/lmod/linux-rhel7-ppc64le/Core
+  module load gcc
+  module load mpich
+  module load cmake
+fi
