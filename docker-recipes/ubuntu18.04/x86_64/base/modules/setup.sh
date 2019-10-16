7792ea8c416d856b3edad4407bef10570c40d1c7
eugeneswalker <eugenesunsetwalker@gmail.com>
2019-10-16 15:54:39 -0700

    update ubuntu1804 base recipe for x86_64

---
+++ docker-recipes/ubuntu18.04/x86_64/base/modules/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+mv modules.yaml /spack/etc/spack/defaults/modules.yaml
+cat bashrc_addendum.sh >> /etc/bash.bashrc
+cat profile_addendum.sh >> /etc/profile
+spack module lmod refresh --delete-tree -y
+sed -i 's/module tcl/module lmod/g' ${SPACK_ROOT}/share/spack/setup-env.sh
