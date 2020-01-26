cdcf44312e686bcfee550bd1e77d6d4f5bc7cff2
eugeneswalker <eugenesunsetwalker@gmail.com>
2020-01-25 22:07:44 -0800

    WIP

--- new-recipes/centos7-x86_64-base/build.sh
+++ new-recipes/centos7-x86_64-base/build.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-
-. env.sh
-
-ts=`date +"%Y-%m-%d.%H%M"`
-bd="docker-context-${ts}"
-rm -rf "${bd}"
-mkdir "${bd}"
-cd "${bd}"
-
-cp ../Dockerfile .
-cp ../config/* .
-
-. pre-process.sh
+set -e
+files=(*)
+cp ../config/pre-process.sh .
+. pre-process.sh ../config
 
 docker build -t ecpe4s/centos7-base-x86_64:0.13.3 .
--- new-recipes/config/pre-process.sh
+++ new-recipes/config/pre-process.sh
@@ -1,8 +1,23 @@
+#!/bin/bash
+set -e
+
+rm -f pre-process.sh
+
+. env.sh 
+
+ts=`date +"%Y-%m-%d.%H%M"`
+bd="docker-context-${ts}"
+rm -rf "${bd}"
+mkdir "${bd}"
+
+cp -r ${files[@]} ${1}/* "${bd}/"
+cd "${bd}"
+
 for f in etc-spack/* module-setup/* spack.yaml ; do
   if [ ! -f "${f}" ] ; then
     continue
   fi
-  sed -i 's@!!TARGETARCH!!@'"${TARGETARCH}"'@' "${f}"
-  sed -i 's/!!CORECOMPILER!!/'"${CORECOMPILER}"'/' "${f}"
-  sed -i 's@!!RCFILE!!@'"${RCFILE}"'@' "${f}"
+  sed -i '' 's@!!TARGETARCH!!@'"${TARGETARCH}"'@' "${f}"
+  sed -i '' 's/!!CORECOMPILER!!/'"${CORECOMPILER}"'/' "${f}"
+  sed -i '' 's@!!RCFILE!!@'"${RCFILE}"'@' "${f}"
 done
