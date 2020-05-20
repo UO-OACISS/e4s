5519f75174a2040be02e43389ea3ee4f7414395a
eugeneswalker <eugenesunsetwalker@gmail.com>
2020-05-20 10:56:14 -0700

    add ubuntu20.04 base recipe for x86_64

---
+++ docker-recipes/ubuntu20.04-spack-x86_64/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -x
+docker build -t ecpe4s/ubuntu20.04-spack-x86_64:0.14.2 \
+ --build-arg SPACK_ROOT=/opt/spack \
+ --build-arg SPACK_MIRROR=https://cache.e4s.io/e4s \
+ --build-arg SPACK_REF=95a03f6759b99701dfa9bd7c299b838057b5a28a .
