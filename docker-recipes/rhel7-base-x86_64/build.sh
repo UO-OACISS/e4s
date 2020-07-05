b940972b7cfdee45142917987e88d89e85d086cd
eugeneswalker <eugenesunsetwalker@gmail.com>
2020-07-05 15:04:56 -0700

    new base image: rhel7 x86_64

--- docker-recipes/rhel7-base-x86_64/build.sh
+++ docker-recipes/rhel7-base-x86_64/build.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker build --no-cache -t ecpe4s/rhel7-base-x86_64:1.1 .
+DATE=`printf '%(%Y%m%d)T' -1`
+REPO=`git config --get remote.origin.url`
+COMMIT=`git log -1 --format="%H"`
+
+docker build \
+  --build-arg DATE=${DATE} \
+  --build-arg REPO=${REPO} \
+  --build-arg COMMIT=${COMMIT} \
+  -t ecpe4s/rhel7-base-x86_64:${DATE} .
