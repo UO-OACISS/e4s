3ed3d6b2464ec0dd386f81ca3f81ad4f5fbd78c3
eugeneswalker <eugenesunsetwalker@gmail.com>
2020-07-05 15:08:05 -0700

    new base image: centos7 x86_64

--- docker-recipes/centos7-base-x86_64/build.sh
+++ docker-recipes/centos7-base-x86_64/build.sh
@@ -1,2 +1,10 @@
-#!/bin/bash -x
-docker build --no-cache -t ecpe4s/centos7-base-x86_64:1.1 .
+#!/bin/bash
+DATE=`printf '%(%Y%m%d)T' -1`
+REPO=`git config --get remote.origin.url`
+COMMIT=`git log -1 --format="%H"`
+
+docker build \
+  --build-arg DATE=${DATE} \
+  --build-arg REPO=${REPO} \
+  --build-arg COMMIT=${COMMIT} \
+  -t ecpe4s/centos7-base-x86_64:${DATE} .
