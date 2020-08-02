d80c1c401a88cc4eba0e62f7ed78f411ece82287
eugeneswalker <eugenesunsetwalker@gmail.com>
2020-08-02 13:37:11 -0700

    new spack ppc64le recipes

--- docker-recipes/centos8-spack-ppc64le/build.sh
+++ docker-recipes/centos8-spack-ppc64le/build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-docker build -t ecpe4s/centos8-spack-ppc64le:0.14.2 \
+
+BUILD_DATE=`printf '%(%Y-%m-%d)T' -1`
+BUILD_REPO=`git config --get remote.origin.url`
+BUILD_REPO_REF=`git log -1 --format="%H"`
+
+docker build \
+ --build-arg BASE_IMAGE=ecpe4s/centos8-runner-ppc64le:2020-08-01 \
+ --build-arg SPACK_REPO=https://github.com/spack/spack.git \
+ --build-arg SPACK_REPO_REF=f3cb3a2eb8991cdfa131abcb429f13903911db1f \
  --build-arg SPACK_ROOT=/opt/spack \
- --build-arg SPACK_MIRROR=https://cache.e4s.io/e4s \
- --build-arg SPACK_REF=95a03f6759b99701dfa9bd7c299b838057b5a28a .
+ --build-arg BUILD_DATE=${BUILD_DATE} \
+ --build-arg BUILD_REPO=${BUILD_REPO} \
+ --build-arg BUILD_REPO_REF=${BUILD_REPO_REF} \
+ -t ecpe4s/centos8-spack-ppc64le:0.15.3 .
