6776b2176515b8c1f044feedfd94b0a7cf2c8186
eugeneswalker <eugenesunsetwalker@gmail.com>
2020-08-01 17:53:19 -0700

    base x86_64 recipes: better labeling

--- docker-recipes/centos8-base-x86_64/build.sh
+++ docker-recipes/centos8-base-x86_64/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-DATE=`printf '%(%Y-%m-%d)T' -1`
-REPO=`git config --get remote.origin.url`
-COMMIT=`git log -1 --format="%H"`
+
+BUILD_DATE=`printf '%(%Y-%m-%d)T' -1`
+BUILD_REPO=`git config --get remote.origin.url`
+BUILD_REPO_REF=`git log -1 --format="%H"`
 
 docker build \
-  --build-arg DATE=${DATE} \
-  --build-arg REPO=${REPO} \
-  --build-arg COMMIT=${COMMIT} \
-  -t ecpe4s/centos8-base-x86_64:${DATE} .
+  --build-arg BUILD_DATE=${BUILD_DATE} \
+  --build-arg BUILD_REPO=${BUILD_REPO} \
+  --build-arg BUILD_REPO_REF=${BUILD_REPO_REF} \
+  -t ecpe4s/centos8-base-x86_64:${BUILD_DATE} .
