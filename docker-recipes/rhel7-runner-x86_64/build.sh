5ce293c5ca95618e83cc2f7835d60bb163affca3
eugeneswalker <eugenesunsetwalker@gmail.com>
2020-08-02 01:15:12 +0000

    add runner recipes for ppc64le and x86_64

---
+++ docker-recipes/rhel7-runner-x86_64/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BUILD_DATE=`printf '%(%Y-%m-%d)T' -1`
+BUILD_REPO=`git config --get remote.origin.url`
+BUILD_REPO_REF=`git log -1 --format="%H"`
+
+docker build --no-cache \
+ --build-arg BASE_IMAGE=ecpe4s/rhel7-base-x86_64:2020-08-01 \
+ --build-arg COMPILER_FROM_SYSTEM="gcc@4.8.5" \
+ --build-arg COMPILER_FROM_SPACK="gcc@8.1.0" \
+ --build-arg SPACK_REPO=https://github.com/spack/spack.git \
+ --build-arg SPACK_REPO_REF=f3cb3a2eb8991cdfa131abcb429f13903911db1f \
+ --build-arg SPACK_ROOT=/opt/spack \
+ --build-arg BUILD_DATE=${BUILD_DATE} \
+ --build-arg BUILD_REPO=${BUILD_REPO} \
+ --build-arg BUILD_REPO_REF=${BUILD_REPO_REF} \
+ -t ecpe4s/rhel7-runner-x86_64:${BUILD_DATE} .
---
+++ docker-recipes/ubuntu20.04-runner-ppc64le/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BUILD_DATE=`printf '%(%Y-%m-%d)T' -1`
+BUILD_REPO=`git config --get remote.origin.url`
+BUILD_REPO_REF=`git log -1 --format="%H"`
+
+docker build --no-cache \
+ --build-arg BASE_IMAGE=ecpe4s/ubuntu20.04-base-ppc64le:2020-08-01 \
+ --build-arg COMPILER_FROM_SYSTEM="gcc@9.3.0" \
+ --build-arg COMPILER_FROM_SPACK="" \
+ --build-arg SPACK_REPO=https://github.com/spack/spack.git \
+ --build-arg SPACK_REPO_REF=f3cb3a2eb8991cdfa131abcb429f13903911db1f \
+ --build-arg SPACK_ROOT=/opt/spack \
+ --build-arg BUILD_DATE=${BUILD_DATE} \
+ --build-arg BUILD_REPO=${BUILD_REPO} \
+ --build-arg BUILD_REPO_REF=${BUILD_REPO_REF} \
+ -t ecpe4s/ubuntu20.04-runner-ppc64le:${BUILD_DATE} .
