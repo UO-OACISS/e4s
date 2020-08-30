cd2ef2639baa688ee9b223ccc64d3d29a0ee3640
eugeneswalker <eugenesunsetwalker@gmail.com>
2020-08-30 20:53:36 +0000

    base recipes: standardize + improve parameterization

--- docker-recipes/ubuntu20.04-base-ppc64le/build.sh
+++ docker-recipes/ubuntu20.04-base-ppc64le/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-IMAGE_NAME="ubuntu20.04-base-ppc64le"
-REGISTRY=${REGISTRY:-ecpe4s}
 BUILD_DATE=$(printf '%(%Y-%m-%d)T' -1)
 BUILD_REPO=$(git config --get remote.origin.url)
 BUILD_REPO_REF=$(git log -1 --format="%H")
+
+REGISTRY=${REGISTRY:-ecpe4s}
+OUTPUT_IMAGE="${REGISTRY}/ubuntu20.04-base-ppc64le:${BUILD_DATE}"
 
 docker build \
   --build-arg BUILD_DATE="${BUILD_DATE}" \
   --build-arg BUILD_REPO="${BUILD_REPO}" \
   --build-arg BUILD_REPO_REF="${BUILD_REPO_REF}" \
-  -t "${REGISTRY}/${IMAGE_NAME}:${BUILD_DATE}" .
+  -t "${OUTPUT_IMAGE}" .
