06b83ff8a82bb08f71b5c7269bef773179d55c11
eugeneswalker <eugenesunsetwalker@gmail.com>
2020-08-30 20:21:56 +0000

    add recipe: ubuntu18.04-runner-arm64

---
+++ docker-recipes/ubuntu18.04-runner-arm64/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BUILD_DATE=$(printf '%(%Y-%m-%d)T' -1)
+BUILD_REPO=$(git config --get remote.origin.url)
+BUILD_REPO_REF=$(git log -1 --format="%H")
+
+REGISTRY=${REGISTRY:-ecpe4s}
+BASE_IMAGE="${BASE_REGISTRY:-$REGISTRY}/ubuntu18.04-base-arm64:2020-08-30"
+OUTPUT_IMAGE="${OUTPUT_REGISTRY:-$REGISTRY}/ubuntu18.04-runner-arm64:${BUILD_DATE}"
+
+docker build \
+ --build-arg BASE_IMAGE="${BASE_IMAGE}" \
+ --build-arg COMPILER_FROM_SYSTEM="gcc@7.5.0" \
+ --build-arg COMPILER_FROM_SPACK="" \
+ --build-arg SPACK_REPO=https://github.com/spack/spack.git \
+ --build-arg SPACK_REPO_REF=f3cb3a2eb8991cdfa131abcb429f13903911db1f \
+ --build-arg SPACK_ROOT=/opt/spack \
+ --build-arg BUILD_DATE="${BUILD_DATE}" \
+ --build-arg BUILD_REPO="${BUILD_REPO}" \
+ --build-arg BUILD_REPO_REF="${BUILD_REPO_REF}" \
+ -t "${OUTPUT_IMAGE}" .
