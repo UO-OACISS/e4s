7a381fe581b1015f3f324543b156772f966e4357
eugeneswalker <eugenesunsetwalker@gmail.com>
2020-08-30 20:41:54 +0000

    runner recipes: standardize build.sh

--- docker-recipes/centos7-runner-ppc64le/build.sh
+++ docker-recipes/centos7-runner-ppc64le/build.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-BUILD_DATE=`printf '%(%Y-%m-%d)T' -1`
-BUILD_REPO=`git config --get remote.origin.url`
-BUILD_REPO_REF=`git log -1 --format="%H"`
+BUILD_DATE=$(printf '%(%Y-%m-%d)T' -1)
+BUILD_REPO=$(git config --get remote.origin.url)
+BUILD_REPO_REF=$(git log -1 --format="%H")
 
-docker build --no-cache \
- --build-arg BASE_IMAGE=ecpe4s/centos7-base-ppc64le:2020-08-01 \
+REGISTRY=${REGISTRY:-ecpe4s}
+BASE_IMAGE="${BASE_REGISTRY:-$REGISTRY}/centos7-base-ppc64le:2020-08-30"
+OUTPUT_IMAGE="${OUTPUT_REGISTRY:-$REGISTRY}/centos7-runner-ppc64le:${BUILD_DATE}"
+
+docker build \
+ --build-arg BASE_IMAGE="${BASE_IMAGE}" \
  --build-arg COMPILER_FROM_SYSTEM="gcc@4.8.5" \
  --build-arg COMPILER_FROM_SPACK="gcc@8.1.0" \
  --build-arg SPACK_REPO=https://github.com/spack/spack.git \
  --build-arg SPACK_REPO_REF=f3cb3a2eb8991cdfa131abcb429f13903911db1f \
  --build-arg SPACK_ROOT=/opt/spack \
- --build-arg BUILD_DATE=${BUILD_DATE} \
- --build-arg BUILD_REPO=${BUILD_REPO} \
- --build-arg BUILD_REPO_REF=${BUILD_REPO_REF} \
- -t ecpe4s/centos7-runner-ppc64le:${BUILD_DATE} .
+ --build-arg BUILD_DATE="${BUILD_DATE}" \
+ --build-arg BUILD_REPO="${BUILD_REPO}" \
+ --build-arg BUILD_REPO_REF="${BUILD_REPO_REF}" \
+ -t "${OUTPUT_IMAGE}" .
--- docker-recipes/rhel8-runner-ppc64le/build.sh
+++ docker-recipes/rhel8-runner-ppc64le/build.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-BUILD_DATE=`printf '%(%Y-%m-%d)T' -1`
-BUILD_REPO=`git config --get remote.origin.url`
-BUILD_REPO_REF=`git log -1 --format="%H"`
+BUILD_DATE=$(printf '%(%Y-%m-%d)T' -1)
+BUILD_REPO=$(git config --get remote.origin.url)
+BUILD_REPO_REF=$(git log -1 --format="%H")
 
-docker build --no-cache \
- --build-arg BASE_IMAGE=ecpe4s/rhel8-base-ppc64le:2020-08-01 \
+REGISTRY=${REGISTRY:-ecpe4s}
+BASE_IMAGE="${BASE_REGISTRY:-$REGISTRY}/rhel8-base-ppc64le:2020-08-30"
+OUTPUT_IMAGE="${OUTPUT_REGISTRY:-$REGISTRY}/rhel8-runner-ppc64le:${BUILD_DATE}"
+
+docker build \
+ --build-arg BASE_IMAGE="${BASE_IMAGE}" \
  --build-arg COMPILER_FROM_SYSTEM="gcc@8.3.1" \
  --build-arg COMPILER_FROM_SPACK="" \
  --build-arg SPACK_REPO=https://github.com/spack/spack.git \
  --build-arg SPACK_REPO_REF=f3cb3a2eb8991cdfa131abcb429f13903911db1f \
  --build-arg SPACK_ROOT=/opt/spack \
- --build-arg BUILD_DATE=${BUILD_DATE} \
- --build-arg BUILD_REPO=${BUILD_REPO} \
- --build-arg BUILD_REPO_REF=${BUILD_REPO_REF} \
- -t ecpe4s/rhel8-runner-ppc64le:${BUILD_DATE} .
+ --build-arg BUILD_DATE="${BUILD_DATE}" \
+ --build-arg BUILD_REPO="${BUILD_REPO}" \
+ --build-arg BUILD_REPO_REF="${BUILD_REPO_REF}" \
+ -t "${OUTPUT_IMAGE}" .
