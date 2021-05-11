f4c78a2ca02c1cd3dc58669f92cbb37e6782616b
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-05-10 17:18:59 -0700

    simplify rhel7-runner recipes

--- docker-recipes/rhel7-runner-ppc64le/build.sh
+++ docker-recipes/rhel7-runner-ppc64le/build.sh
@@ -5,18 +5,12 @@ BUILD_REPO=$(git config --get remote.origin.url)
 BUILD_REPO_REF=$(git log -1 --format="%H")
 
 REGISTRY=${REGISTRY:-ecpe4s}
-BASE_IMAGE_TAG=${BASE_IMAGE_TAG:-$BUILD_DATE}
-BASE_IMAGE="${BASE_REGISTRY:-$REGISTRY}/rhel7-base-ppc64le:${BASE_IMAGE_TAG}"
-OUTPUT_IMAGE="${OUTPUT_REGISTRY:-$REGISTRY}/rhel7-runner-ppc64le:${BUILD_DATE}"
+OUTPUT_IMAGE="${REGISTRY}/rhel7-runner-ppc64le:${BUILD_DATE}"
 
 docker build \
- --build-arg BASE_IMAGE="${BASE_IMAGE}" \
- --build-arg COMPILER_FROM_SYSTEM="gcc@4.8.5" \
- --build-arg COMPILER_FROM_SPACK="gcc@8.1.0" \
- --build-arg SPACK_REPO=https://github.com/spack/spack.git \
- --build-arg SPACK_REPO_REF=55eae72bb0a2c58cd15fc43d8ca23e90cd84cdfe \
- --build-arg SPACK_ROOT=/opt/spack \
- --build-arg BUILD_DATE="${BUILD_DATE}" \
- --build-arg BUILD_REPO="${BUILD_REPO}" \
- --build-arg BUILD_REPO_REF="${BUILD_REPO_REF}" \
- -t "${OUTPUT_IMAGE}" .
+  --build-arg BUILD_DATE="${BUILD_DATE}" \
+  --build-arg BUILD_REPO="${BUILD_REPO}" \
+  --build-arg BUILD_REPO_REF="${BUILD_REPO_REF}" \
+  --build-arg SPACK_REPO=https://github.com/spack/spack.git \
+  --build-arg SPACK_REPO_REF=3d2b1277e973b7267c9200631e24a6df38ff9b4b \
+  -t "${OUTPUT_IMAGE}" .
--- docker-recipes/rhel7-runner-x86_64/build.sh
+++ docker-recipes/rhel7-runner-x86_64/build.sh
@@ -5,18 +5,12 @@ BUILD_REPO=$(git config --get remote.origin.url)
 BUILD_REPO_REF=$(git log -1 --format="%H")
 
 REGISTRY=${REGISTRY:-ecpe4s}
-BASE_IMAGE_TAG=${BASE_IMAGE_TAG:-$BUILD_DATE}
-BASE_IMAGE="${BASE_REGISTRY:-$REGISTRY}/rhel7-base-x86_64:${BASE_IMAGE_TAG}"
-OUTPUT_IMAGE="${OUTPUT_REGISTRY:-$REGISTRY}/rhel7-runner-x86_64:${BUILD_DATE}"
+OUTPUT_IMAGE="${REGISTRY}/rhel7-runner-x86_64:${BUILD_DATE}"
 
 docker build \
- --build-arg BASE_IMAGE="${BASE_IMAGE}" \
- --build-arg COMPILER_FROM_SYSTEM="gcc@4.8.5" \
- --build-arg COMPILER_FROM_SPACK="gcc@8.1.0" \
- --build-arg SPACK_REPO=https://github.com/spack/spack.git \
- --build-arg SPACK_REPO_REF=e25a7be5b89b1bcf550accb4de8314cd9b0989db \
- --build-arg SPACK_ROOT=/opt/spack \
- --build-arg BUILD_DATE="${BUILD_DATE}" \
- --build-arg BUILD_REPO="${BUILD_REPO}" \
- --build-arg BUILD_REPO_REF="${BUILD_REPO_REF}" \
- -t "${OUTPUT_IMAGE}" .
+  --build-arg BUILD_DATE="${BUILD_DATE}" \
+  --build-arg BUILD_REPO="${BUILD_REPO}" \
+  --build-arg BUILD_REPO_REF="${BUILD_REPO_REF}" \
+  --build-arg SPACK_REPO=https://github.com/spack/spack.git \
+  --build-arg SPACK_REPO_REF=3d2b1277e973b7267c9200631e24a6df38ff9b4b \
+  -t "${OUTPUT_IMAGE}" .
