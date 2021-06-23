b707dd6d06880e394b089781d418d89af09cc985
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-06-22 23:44:50 -0400

    update ubuntu 18.04 runner recipes

--- docker-recipes/ubuntu18.04-runner-ppc64le/build.sh
+++ docker-recipes/ubuntu18.04-runner-ppc64le/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 BUILD_DATE=$(printf '%(%Y-%m-%d)T' -1)
+BUILD_TAG=${BUILD_TAG:-${BUILD_DATE}}
 BUILD_REPO=$(git config --get remote.origin.url)
 BUILD_REPO_REF=$(git log -1 --format="%H")
 
 REGISTRY=${REGISTRY:-ecpe4s}
-OUTPUT_IMAGE="${REGISTRY}/ubuntu18.04-runner-ppc64le:${BUILD_DATE}"
+OUTPUT_IMAGE="${REGISTRY}/ubuntu18.04-runner-ppc64le:${BUILD_TAG}"
 
 docker build \
   --build-arg BUILD_DATE="${BUILD_DATE}" \
