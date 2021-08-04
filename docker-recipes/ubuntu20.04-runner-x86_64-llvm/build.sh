5dea3bc8fd05f5e1f492354622ec448b43204213
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-08-03 21:02:22 -0700

    update recipe

--- docker-recipes/ubuntu20.04-runner-x86_64-llvm/build.sh
+++ docker-recipes/ubuntu20.04-runner-x86_64-llvm/build.sh
@@ -6,10 +6,12 @@ BUILD_REPO=$(git config --get remote.origin.url)
 BUILD_REPO_REF=$(git log -1 --format="%H")
 
 REGISTRY=${REGISTRY:-ecpe4s}
-OUTPUT_IMAGE="${REGISTRY}/ubuntu20.04-runner-x86_64:${BUILD_TAG}"
+OUTPUT_IMAGE="${REGISTRY}/ubuntu20.04-runner-x86_64:${BUILD_TAG}-llvm"
 
 docker build \
   --build-arg BUILD_DATE="${BUILD_DATE}" \
   --build-arg BUILD_REPO="${BUILD_REPO}" \
   --build-arg BUILD_REPO_REF="${BUILD_REPO_REF}" \
+  --build-arg SPACK_REPO=https://github.com/spack/spack.git \
+  --build-arg SPACK_REPO_REF=4e9bccf2ef3f5433cb1bfb12d45bcfe64f1a7eb0 \
   -t "${OUTPUT_IMAGE}" .
