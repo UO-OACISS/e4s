6bc0d3bee9c3eafd43980fa146bb1e440d9a5ec4
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-08-27 11:52:41 -0700

    rhel7 x86_64: use gcc 11.2

--- docker-recipes/rhel7-runner-x86_64/build.sh
+++ docker-recipes/rhel7-runner-x86_64/build.sh
@@ -6,14 +6,12 @@ BUILD_REPO=$(git config --get remote.origin.url)
 BUILD_REPO_REF=$(git log -1 --format="%H")
 
 REGISTRY=${REGISTRY:-ecpe4s}
-OUTPUT_IMAGE="${REGISTRY}/rhel7-runner-x86_64:${BUILD_TAG}"
+OUTPUT_IMAGE="${REGISTRY}/rhel7-runner-x86_64:${BUILD_TAG}-gcc11.2"
 
 docker build \
   --build-arg BUILD_DATE="${BUILD_DATE}" \
   --build-arg BUILD_REPO="${BUILD_REPO}" \
   --build-arg BUILD_REPO_REF="${BUILD_REPO_REF}" \
   --build-arg SPACK_REPO=https://github.com/spack/spack.git \
-  --build-arg SPACK_REPO_REF=2db858e9c4fc16db4051004e4cca8b11154a3d22 \
+  --build-arg SPACK_REPO_REF=4f3a5385198db52b7fdcf860de4a35993cbb52e3 \
   -t "${OUTPUT_IMAGE}" .
-
- #d19d81a3f4451af5911ad46d24754268ba5137d6 \
