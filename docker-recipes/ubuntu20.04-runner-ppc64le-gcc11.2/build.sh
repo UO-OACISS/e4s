2fce5326123e95414a3e4d43330c40695eefe79c
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-08-28 00:43:31 -0400

    add ubuntu20.04 ppc64le runner w gcc@11.2.0

---
+++ docker-recipes/ubuntu20.04-runner-ppc64le-gcc11.2/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BUILD_DATE=$(printf '%(%Y-%m-%d)T' -1)
+BUILD_TAG=${BUILD_TAG:-${BUILD_DATE}}
+BUILD_REPO=$(git config --get remote.origin.url)
+BUILD_REPO_REF=$(git log -1 --format="%H")
+
+REGISTRY=${REGISTRY:-ecpe4s}
+OUTPUT_IMAGE="${REGISTRY}/ubuntu20.04-runner-ppc64le:${BUILD_TAG}-gcc11.2"
+
+docker build \
+  --build-arg BUILD_DATE="${BUILD_DATE}" \
+  --build-arg BUILD_REPO="${BUILD_REPO}" \
+  --build-arg BUILD_REPO_REF="${BUILD_REPO_REF}" \
+  --build-arg SPACK_REPO=https://github.com/spack/spack.git \
+  --build-arg SPACK_REPO_REF=4f3a5385198db52b7fdcf860de4a35993cbb52e3 \
+  -t "${OUTPUT_IMAGE}" .
