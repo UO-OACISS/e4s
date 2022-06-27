3433e61995e785c3b06b4eec9d873b587ebec5e0
eugeneswalker <eugenesunsetwalker@gmail.com>
2022-06-27 10:37:53 -0400

    add power recipe

---
+++ docker-recipes/minimal/ubuntu20.04-ppc64le/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BUILD_DATE=$(printf '%(%Y-%m-%d)T' -1)
+BUILD_TAG=${BUILD_TAG:-${BUILD_DATE}}
+BUILD_REPO=$(git config --get remote.origin.url)
+BUILD_REPO_REF=$(git log -1 --format="%H")
+
+REGISTRY=${REGISTRY:-ecpe4s}
+OUTPUT_IMAGE="${REGISTRY}/ubuntu20.04-ppc64le:${BUILD_TAG}"
+
+docker build -t "${OUTPUT_IMAGE}" .
