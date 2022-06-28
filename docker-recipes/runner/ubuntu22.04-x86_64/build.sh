a2ebec1892e1a48aa50f10f296ed6981561d01cd
eugeneswalker <eugenesunsetwalker@gmail.com>
2022-06-27 19:23:05 -0700

    update runner recipes

--- docker-recipes/runner/ubuntu22.04-x86_64/build.sh
+++ docker-recipes/runner/ubuntu22.04-x86_64/build.sh
@@ -2,14 +2,8 @@
 
 BUILD_DATE=$(printf '%(%Y-%m-%d)T' -1)
 BUILD_TAG=${BUILD_TAG:-${BUILD_DATE}}
-BUILD_REPO=$(git config --get remote.origin.url)
-BUILD_REPO_REF=$(git log -1 --format="%H")
 
 REGISTRY=${REGISTRY:-ecpe4s}
 OUTPUT_IMAGE="${REGISTRY}/ubuntu22.04-runner-x86_64:${BUILD_TAG}"
 
-docker build \
-  --build-arg BUILD_DATE="${BUILD_DATE}" \
-  --build-arg BUILD_REPO="${BUILD_REPO}" \
-  --build-arg BUILD_REPO_REF="${BUILD_REPO_REF}" \
-  -t "${OUTPUT_IMAGE}" .
+docker build -t "${OUTPUT_IMAGE}" .
