b7cfa10a11d39871b5abb0274120507cb32b9f04
eugeneswalker <eugenesunsetwalker@gmail.com>
2022-07-11 18:25:34 +0000

    update ubuntu 22.04 aarch64 mimimal recipe

--- docker-recipes/minimal/ubuntu22.04-aarch64/build.sh
+++ docker-recipes/minimal/ubuntu22.04-aarch64/build.sh
@@ -2,8 +2,6 @@
 
 BUILD_DATE=$(printf '%(%Y-%m-%d)T' -1)
 BUILD_TAG=${BUILD_TAG:-${BUILD_DATE}}
-BUILD_REPO=$(git config --get remote.origin.url)
-BUILD_REPO_REF=$(git log -1 --format="%H")
 
 REGISTRY=${REGISTRY:-ecpe4s}
 OUTPUT_IMAGE="${REGISTRY}/ubuntu22.04-aarch64:${BUILD_TAG}"
