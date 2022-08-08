37ae613055b10d8a833bc5694278834c750a2cf4
eugeneswalker <eugenesunsetwalker@gmail.com>
2022-08-08 03:10:42 +0000

    add rhel8 minimal recipe for aarch64

---
+++ docker-recipes/minimal/rhel8-aarch64/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BUILD_DATE=$(printf '%(%Y-%m-%d)T' -1)
+BUILD_TAG=${BUILD_TAG:-${BUILD_DATE}}
+BUILD_REPO=$(git config --get remote.origin.url)
+BUILD_REPO_REF=$(git log -1 --format="%H")
+
+REGISTRY=${REGISTRY:-ecpe4s}
+OUTPUT_IMAGE="${REGISTRY}/rhel8-aarch64:${BUILD_TAG}"
+
+docker build -t "${OUTPUT_IMAGE}" .
