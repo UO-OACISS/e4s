dd6486f097677141fcc72054c0383e869d6bc8e3
eugeneswalker <eugenesunsetwalker@gmail.com>
2022-11-10 12:22:50 -0800

    update ubuntu20.04-ppc64le minimal recipe

--- docker-recipes/minimal/ubuntu20.04-ppc64le/entry.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. /spack/share/spack/setup-env.sh
-. /bootstrap/runner/install/linux-ubuntu20.04-ppc64le/gcc-9.4.0/lmod-8.7.2-43hnxjbtyc4f6owoy6763jvuzvtx6u64/lmod/8.7.2/init/bash
-module use /spack/share/spack/lmod/linux-ubuntu20.04-ppc64le/Core
-module load mpich
-module load cmake
-exec $@
