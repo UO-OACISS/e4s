a3d3ff1fd795298accfb1d3cefc4cb14408c558d
eugeneswalker <eugenesunsetwalker@gmail.com>
2023-06-06 04:20:11 +0000

    add label as gcc-12.3

--- docker-recipes/runner/ubuntu20.04-arm64-gcc-11.4.0/build.sh
+++ docker-recipes/runner/ubuntu20.04-arm64-gcc-11.4.0/build.sh
@@ -4,6 +4,6 @@ BUILD_DATE=$(printf '%(%Y.%m.%d)T' -1)
 BUILD_TAG=${BUILD_TAG:-${BUILD_DATE}}
 
 REGISTRY=${REGISTRY:-ecpe4s}
-OUTPUT_IMAGE="${REGISTRY}/ubuntu20.04-runner-arm64-gcc-12:${BUILD_TAG}"
+OUTPUT_IMAGE="${REGISTRY}/ubuntu20.04-runner-arm64-gcc-12.3:${BUILD_TAG}"
 
 docker build -t "${OUTPUT_IMAGE}" .
