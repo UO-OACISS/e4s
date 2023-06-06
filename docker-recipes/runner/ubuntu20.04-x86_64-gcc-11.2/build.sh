27d0ff2fdc888474077a1b0725d8a3d540407e90
eugeneswalker <eugenesunsetwalker@gmail.com>
2023-06-06 08:45:09 -0700

    update recipes

--- docker-recipes/runner/ubuntu20.04-x86_64-gcc-11.2/build.sh
+++ docker-recipes/runner/ubuntu20.04-x86_64-gcc-11.2/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-BUILD_DATE=$(printf '%(%Y-%m-%d)T' -1)
+BUILD_DATE=$(printf '%(%Y.%m.%d)T' -1)
 BUILD_TAG=${BUILD_TAG:-${BUILD_DATE}}
 
 REGISTRY=${REGISTRY:-ecpe4s}
-OUTPUT_IMAGE="${REGISTRY}/ubuntu20.04-runner-x86_64-gcc-12:${BUILD_TAG}"
+OUTPUT_IMAGE="${REGISTRY}/ubuntu20.04-runner-x86_64-gcc-11.2:${BUILD_TAG}"
 
 docker build -t "${OUTPUT_IMAGE}" .
