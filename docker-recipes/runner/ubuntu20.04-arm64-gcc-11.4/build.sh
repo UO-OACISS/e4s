395dc0dcf979b05d0c215c7c67f8a2b17c84f36a
eugeneswalker <eugenesunsetwalker@gmail.com>
2023-08-04 13:40:38 +0000

    update arm gcc 11.4 runner build.sh

--- docker-recipes/runner/ubuntu20.04-arm64-gcc-11.4/build.sh
+++ docker-recipes/runner/ubuntu20.04-arm64-gcc-11.4/build.sh
@@ -1,11 +1,23 @@
-#!/bin/bash
+#!/bin/bash -e
+
+if [[ $# -ne 1 || ( "$1" != "base" && "$1" != "complete" ) ]] ; then
+  echo "usage: $0 <base|complete>"
+  exit 1
+fi
+
+BUILD_TYPE=$1
 
 BUILD_DATE=$(printf '%(%Y.%m.%d)T' -1)
 BUILD_TAG=${BUILD_TAG:-${BUILD_DATE}}
-BUILD_REPO=$(git config --get remote.origin.url)
-BUILD_REPO_REF=$(git log -1 --format="%H")
+BUILD_NAME=ubuntu20.04-runner-arm64-gcc-11.4
+if [[ "$BUILD_TYPE" == "base" ]]; then
+  BUILD_NAME=${BUILD_NAME}-base
+fi
 
 REGISTRY=${REGISTRY:-ecpe4s}
-OUTPUT_IMAGE="${REGISTRY}/ubuntu20.04-runner-aarch64-gcc-11.4:${BUILD_TAG}"
+OUTPUT_IMAGE="${REGISTRY}/${BUILD_NAME}:${BUILD_TAG}"
 
-docker build -t "${OUTPUT_IMAGE}" .
+docker build \
+ --target ${BUILD_TYPE} \
+ -t "${OUTPUT_IMAGE}" \
+ .
