3c1222e53050ce5d22fc231dafe0aa50f2b76724
eugeneswalker <eugenesunsetwalker@gmail.com>
2023-10-11 08:58:54 -0700

    annotate oneapi compiler version

---
+++ docker-recipes/runner/ubuntu20.04-amd64-oneapi-2023.2.1/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+if [[ $# -ne 1 || ( "$1" != "base" && "$1" != "complete" ) ]] ; then
+  echo "usage: $0 <base|complete>"
+  exit 1
+fi
+
+BUILD_TYPE=$1
+
+BUILD_DATE=$(printf '%(%Y.%m.%d)T' -1)
+BUILD_TAG=${BUILD_TAG:-${BUILD_DATE}}
+BUILD_NAME=ubuntu20.04-runner-amd64-oneapi-2023.2.1
+if [[ "$BUILD_TYPE" == "base" ]]; then
+  BUILD_NAME=${BUILD_NAME}-base
+fi
+
+REGISTRY=${REGISTRY:-ecpe4s}
+OUTPUT_IMAGE="${REGISTRY}/${BUILD_NAME}:${BUILD_TAG}"
+
+docker build \
+ --target ${BUILD_TYPE} \
+ -t "${OUTPUT_IMAGE}" \
+ .
