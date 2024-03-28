5c5ff356b9fbeee5f3a86761ce5ca5c2ef08512f
eugeneswalker <eugenesunsetwalker@gmail.com>
2024-03-28 10:00:57 -0700

    remove base,complete distinction for manylinux2014

--- docker-recipes/runner/centos7-amd64-manylinux2014-gcc-10.2.1/build.sh
+++ docker-recipes/runner/centos7-amd64-manylinux2014-gcc-10.2.1/build.sh
@@ -1,23 +1,12 @@
 #!/bin/bash -e
 
-if [[ $# -ne 1 || ( "$1" != "base" && "$1" != "complete" ) ]] ; then
-  echo "usage: $0 <base|complete>"
-  exit 1
-fi
-
-BUILD_TYPE=$1
-
 BUILD_DATE=$(printf '%(%Y.%m.%d)T' -1)
 BUILD_TAG=${BUILD_TAG:-${BUILD_DATE}}
 BUILD_NAME=manylinux2014
-if [[ "$BUILD_TYPE" == "base" ]]; then
-  BUILD_NAME=${BUILD_NAME}-base
-fi
 
 REGISTRY=${REGISTRY:-ecpe4s}
 OUTPUT_IMAGE="${REGISTRY}/${BUILD_NAME}:${BUILD_TAG}"
 
 docker build \
- --target ${BUILD_TYPE} \
  -t "${OUTPUT_IMAGE}" \
  .
