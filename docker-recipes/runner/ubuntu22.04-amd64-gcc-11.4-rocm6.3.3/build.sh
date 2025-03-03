03320bb1ef50f0b9c80b258dd15340ee66f3ece6
eugeneswalker <eugenesunsetwalker@gmail.com>
2025-03-03 08:59:58 -0800

    add rocm6.3.{2,3} runner recipes

---
+++ docker-recipes/runner/ubuntu22.04-amd64-gcc-11.4-rocm6.3.3/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+BUILD_DATE=$(printf '%(%Y.%m.%d)T' -1)
+BUILD_TAG=${BUILD_TAG:-${BUILD_DATE}}
+BUILD_NAME=ubuntu22.04-runner-amd64-gcc-11.4-rocm6.3.3
+
+REGISTRY=${REGISTRY:-ecpe4s}
+OUTPUT_IMAGE="${REGISTRY}/${BUILD_NAME}:${BUILD_TAG}"
+
+docker build \
+ -t "${OUTPUT_IMAGE}" \
+ .
