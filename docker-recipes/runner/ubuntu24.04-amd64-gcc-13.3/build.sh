3c0c119acd17a48ed9319a604f5e5f3bdc345579
eugeneswalker <eugenesunsetwalker@gmail.com>
2025-08-12 13:24:50 -0700

    add ubuntu24.04-amd64 recipe

---
+++ docker-recipes/runner/ubuntu24.04-amd64-gcc-13.3/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+if [[ $# -ne 1 || ( "$1" != "base" && "$1" != "complete" ) ]] ; then
+  echo "usage: $0 <base|complete>"
+  exit 1
+fi
+
+BUILD_TYPE=$1
+
+BUILD_DATE=$(printf '%(%Y.%m.%d)T' -1)
+BUILD_TAG=${BUILD_TAG:-${BUILD_DATE}}
+BUILD_NAME=ubuntu24.04-runner-amd64-gcc-13.3
+if [[ "$BUILD_TYPE" == "base" ]]; then
+  BUILD_NAME=${BUILD_NAME}-base
+fi
+
+REGISTRY=${REGISTRY:-ecpe4s}
+OUTPUT_IMAGE="${REGISTRY}/${BUILD_NAME}:${BUILD_TAG}"
+
+SPACK_ROOT=/spack
+SPACK_REPO=https://github.com/spack/spack
+SPACK_CHECKOUT=e46416596535ef0fcd8ecabdac36b0e332e82785
+# SPACK_MIRROR=https://cache.e4s.io/spack-universal-bootstrap/x86_64_v3-glibc2.17
+
+docker build \
+ --target ${BUILD_TYPE} \
+ -t "${OUTPUT_IMAGE}" \
+ --build-arg SPACK_ROOT=$SPACK_ROOT \
+ --build-arg SPACK_REPO=$SPACK_REPO \
+ --build-arg SPACK_CHECKOUT=$SPACK_CHECKOUT \
+ .
+
+#  --build-arg SPACK_MIRROR=$SPACK_MIRROR \
